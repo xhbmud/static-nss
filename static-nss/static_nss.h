14cdb240b6cf7daa8a8e63ec5bc128395ce479f8
Chang Jun <EricChang@localhost.(none)>
2013-06-28 03:03:16 +0800

    Initial Commit

---
+++ static-nss/static_nss.h
@@ -0,0 +1,13 @@
+//
+//  static_nss.h
+//  static-nss
+//
+//  Created by Chang Jun on 13-6-28.
+//  Copyright (c) 2013年 Chang Jun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface static_nss : NSObject
+
+@end
---
+++ static-nss/static_nss.m
@@ -0,0 +1,13 @@
+//
+//  static_nss.m
+//  static-nss
+//
+//  Created by Chang Jun on 13-6-28.
+//  Copyright (c) 2013年 Chang Jun. All rights reserved.
+//
+
+#import "static_nss.h"
+
+@implementation static_nss
+
+@end
